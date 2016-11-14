045a57695f343c9cb14282775f90e7db96b414d2
ALEXQIANG <ALEXQIANG@wifi-roaming-128-4-221-17.host.udel.edu>
2016-11-13 22:43:11 -0500

    lab5

--- RioClient/src/main/java/poker/app/view/PokerTableController.java
+++ RioClient/src/main/java/poker/app/view/PokerTableController.java
@@ -1,6 +1,8 @@
 package poker.app.view;
 
 import java.net.URL;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 import javafx.animation.FadeTransition;
@@ -130,6 +132,81 @@ public class PokerTableController implements Initializable {
 
 		// Send the Action to the Hub
 		mainApp.messageSend(act);
+	}
+	
+	public void Handle_TableState(Table HubPokerTable) {
+
+		lblPos1Name.setText("");
+		lblPos2Name.setText("");
+		lblPos3Name.setText("");
+		lblPos4Name.setText("");
+
+		//scanInputControls(OuterBorderPane, "SitLeave",true);
+		
+		btnPos1SitLeave.setVisible(true);
+		btnPos2SitLeave.setVisible(true);
+		btnPos3SitLeave.setVisible(true);
+		btnPos4SitLeave.setVisible(true);
+
+		btnPos1SitLeave.setText(btnPos1SitLeave.isSelected() ? "Leave" : "Sit");
+		btnPos2SitLeave.setText(btnPos2SitLeave.isSelected() ? "Leave" : "Sit");
+		btnPos3SitLeave.setText(btnPos3SitLeave.isSelected() ? "Leave" : "Sit");
+		btnPos4SitLeave.setText(btnPos4SitLeave.isSelected() ? "Leave" : "Sit");
+
+		btnStartGame.setDisable(HubPokerTable.getHashPlayers().size() > 0 ? false : true);
+
+		FadeButton(btnStartGame);
+		Iterator it = HubPokerTable.getHashPlayers().entrySet().iterator();
+		while (it.hasNext()) {
+			Map.Entry pair = (Map.Entry) it.next();
+			Player p = (Player) pair.getValue();
+			switch (p.getiPlayerPosition()) {
+			case 1:
+				if (p.getPlayerID().equals(mainApp.getPlayer().getPlayerID())) {
+					btnPos1SitLeave.setVisible(true);
+					btnPos2SitLeave.setVisible(false);
+					btnPos3SitLeave.setVisible(false);
+					btnPos4SitLeave.setVisible(false);
+				} else {
+					btnPos1SitLeave.setVisible(false);
+				}
+				lblPos1Name.setText(p.getPlayerName().toString());
+				break;
+			case 2:
+				if (p.getPlayerID().equals(mainApp.getPlayer().getPlayerID())) {
+					btnPos1SitLeave.setVisible(false);
+					btnPos2SitLeave.setVisible(true);
+					btnPos3SitLeave.setVisible(false);
+					btnPos4SitLeave.setVisible(false);
+				} else {
+					btnPos2SitLeave.setVisible(false);
+				}
+				lblPos2Name.setText(p.getPlayerName().toString());
+				break;
+			case 3:
+				if (p.getPlayerID().equals(mainApp.getPlayer().getPlayerID())) {
+					btnPos1SitLeave.setVisible(false);
+					btnPos2SitLeave.setVisible(false);
+					btnPos3SitLeave.setVisible(true);
+					btnPos4SitLeave.setVisible(false);
+				} else {
+					btnPos3SitLeave.setVisible(false);
+				}
+				lblPos3Name.setText(p.getPlayerName().toString());
+				break;
+			case 4:
+				if (p.getPlayerID().equals(mainApp.getPlayer().getPlayerID())) {
+					btnPos1SitLeave.setVisible(false);
+					btnPos2SitLeave.setVisible(false);
+					btnPos3SitLeave.setVisible(false);
+					btnPos4SitLeave.setVisible(true);
+				} else {
+					btnPos4SitLeave.setVisible(false);
+				}
+				lblPos4Name.setText(p.getPlayerName().toString());
+				break;
+			}
+		}
 	}
 
 	public void MessageFromMainApp(String strMessage) {
